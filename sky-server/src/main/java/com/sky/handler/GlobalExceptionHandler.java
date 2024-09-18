948d2e4249c9f8a2cea5bfa6cf853d4b3039e8a8
Cjeat <1374757281@qq.com>
2024-09-18 10:58:22 +0800

    异常捕捉

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Arrays;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -34,7 +36,13 @@ public class GlobalExceptionHandler {
         log.error("异常信息：{}", ex.getMessage());
 //        "asd qwe 123".split(" ");
 //        ex.getMessage().contanins("Duplicate entry");
-        return Result.error(ex.getMessage());
+        if(ex.getMessage().contains("Duplicate entry")){
+            String[] split = ex.getMessage().split(" ");
+            System.out.println(Arrays.toString(split));
+            String name = split[2];
+            return Result.error(name + MessageConstant.ALREDY_EXISTS);
+        }
+        return Result.error(MessageConstant.UNKNOWN_ERROR);
     }
 
 
